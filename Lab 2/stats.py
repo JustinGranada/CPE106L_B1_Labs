bad3a08a6e578baff5ef03e8125a28398ff3392b
Joshua8032003 <161097202+Joshua8032003@users.noreply.github.com>
2024-04-20 09:57:53 +0800

    Update stats.py

--- Lab 2/stats.py
+++ Lab 2/stats.py
@@ -3,4 +3,10 @@ from collection import number
 def mean(numbers):
   if not numbers:
     return 0
-    return sum(numbers) / len(numbers)
+    
+  return sum(numbers) / len(numbers)
+
+def median(numbers):
+  if not numbers: 
+    return 0
+    sorted_numbers = sorted(numbers)
